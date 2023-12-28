fa94d2197e86509e7f34eaf665e8a890e62538ea
Fellipe Lorram <fellipelorram@gmail.com>
2023-12-28 19:44:56 -0300

    Update topbar shopping cart icon styling

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -5,7 +5,7 @@ export function Topbar() {
 		<div className="flex justify-between items-center p-10 px-20 border-b border-solid border-gray-300 topbar-wrapper">
 			<h1>Logo</h1>
 
-			<div className="relative p-6 rounded-full h-15 w-15 bg-gray-300 bg-opacity-90 flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-gray-300 shopping-cart-icon-wrapper ">
+			<div className="relative p-6 rounded-full h-15 w-15 bg-gray-300 bg-opacity-90 flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-gray-300">
 				<ShoppingCart
 					className="w-5 h-5 shopping-cart-icon"
 				/>
