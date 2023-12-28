d5826d4d15b4a516d865e40a6d785d07facbeb01
Ricardo <galantedmax@gmail.com>
2023-12-28 18:51:51 -0300

    topbar

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -2,12 +2,12 @@ import { ShoppingCart } from "lucide-react";
 
 export function Topbar() {
 	return (
-		<div className="flex justify-between topbar-wrapper">
+		<div className="flex justify-between items-center p-10 px-20 border-b border-solid border-gray-300 topbar-wrapper">
 			<h1>Logo</h1>
 
-			<div className="shopping-cart-icon-wrapper">
+			<div className="relative p-6 rounded-full h-15 w-15 bg-gray-300 bg-opacity-90 flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-gray-300 shopping-cart-icon-wrapper ">
 				<ShoppingCart
-					className="shopping-cart-icon"
+					className="w-5 h-5 shopping-cart-icon"
 				/>
 			</div>
 			
